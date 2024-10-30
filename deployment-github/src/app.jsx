c32d7311c966d94c6ce41a009b3cfe4929db4d02
xKrompo <mikulaswolf@gmail.com>
2024-10-30 10:43:18 +0100

    c

---
+++ deployment-github/src/app.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'preact/hooks'
+
+import './app.css'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+       <p>Deploying on github</p>
+      </div>
+     
+    </>
+  )
+}
